ec965563934a9ea9af33770932733ea17d33f30f
Liza Kravchenko <ekravchenko2019@gmail.com>
2018-08-06 11:12:26 -0700

    createEvent works!

--- src/components/CreateEvent.js
+++ src/components/CreateEvent.js
@@ -7,8 +7,6 @@ import {
     DatesRangeInput
   } from 'semantic-ui-calendar-react';
 
-  //hi
-
   export class CreateEvent extends React.Component {
     constructor(props) {
       super(props);
@@ -25,9 +23,8 @@ import {
         city: '',
         state: '',
         country: '',
-        about: '',
-        latitude: null,
-        longitutde: null,
+        about: ''
+
       };
     }
 
@@ -37,131 +34,128 @@ import {
       }
     }
 
-
+  //hi
   onCreate = () => {
-    console.log('ON CREATE*********************',this.state)
-    this.props.socket.emit('createEvent', {
-        eventName: this.state.eventName,
-        eventCreator: this.state.eventCreator,
-        venueName: this.state.venueName,
-        date: this.state.date,
-        time: this.state.time,
-        datesRange: this.state.datesRange,
-        streetAddress: this.state.streetAddress,
-        city: this.state.city,
-        state: this.state.state,
-        country: this.state.country,
-        about: this.state.about
-    }, (res) => {
-      console.log(res)
-      if(res.err) {
-        return alert('Opps Error')
-      }else{
-        alert('Saved')
-      }
+      // console.log('ON CREATE*********************',this.state)
+      this.props.socket.emit('createEvent', {
+          eventName: this.state.eventName,
+          eventCreator: this.state.eventCreator,
+          venueName: this.state.venueName,
+          date: this.state.date,
+          time: this.state.time,
+          datesRange: this.state.datesRange,
+          streetAddress: this.state.streetAddress,
+          city: this.state.city,
+          state: this.state.state,
+          country: this.state.country,
+          about: this.state.about
+      }, (res) => {
+        console.log(res)
+        if(res.err) {
+          return alert('Opps Error')
+        }else{
+          alert('Saved')
+        }
 
-    })
-  }
-
-
-  // onUsernameChange = (event) =>{
-  //   this.setState({
-  //     username: event.target.value
-  //   })
-  // }
-    render() {
-      return (
-
-        <Form>
-          <Form.Group>
-            <Form.Field control={Input} label='Event Name' placeholder='Event Name' onChange={this.eventName} />
-            <Form.Field control={Input} label='Event Creator' placeholder='Event Creator' onChange={this.eventCreator} />
-          </ Form.Group>
-
-          <DateInput
-            name="date"
-            inline
-            placeholder="Date"
-            value={this.state.date}
-            iconPosition="left"
-            onChange={this.handleChange} />
-            <br />
-          <TimeInput
-            inline
-            name="time"
-            placeholder="Time"
-            value={this.state.time}
-            iconPosition="left"
-            onChange={this.handleChange} />
-             <br />
-
-          <DatesRangeInput
-            inline
-            name="datesRange"
-            placeholder="From - To"
-            value={this.state.datesRange}
-            iconPosition="left"
-            onChange={this.handleChange} />
-            <Form.Group>
-           <Form.Field control={Input} label='Street Address' placeholder='Street Address' onChange={this.streetAddress} />
-             <Form.Field control={Input} label='City' placeholder='City' onChange={this.city}/>
-             <br />
-             <br />
-             <Form.Field control={Input} label='State' placeholder='State'  onChange={this.state}/>
-             <Form.Field control={Input} label='Country' placeholder='Country' onChange={this.country}/>
-             <Form.Field control={Input} label='Venue Name' placeholder='Venue Name' onChange={this.venueName}/>
-             <Form.Field control={TextArea} label='About' placeholder='Tell us more about you...' onChange={this.about} />
-            </Form.Group>
-            <Button style={{display:'flex', alignItems:'center'}} color = 'pink' className = "logout-button"  animated onClick = {this.onCreate}>
-             <Button.Content visible>Create Event Go!</Button.Content>
-                 <Button.Content hidden>
-                     <Icon name='right arrow'   />
-                 </Button.Content>
-             </Button>
-        </Form>
-      );
+      })
     }
-  }
 
+    onEventNameChange = (event) => {
+      this.setState({
+        eventName: event.target.value
+      })
+    }
 
-// export class CreateEvent extends Component {
-//     constructor(props){
-//         super(props)
-//         this.state = {}
-//     }
+    onEventCreatorChange = (event) => {
+      this.setState({
+        eventCreator: event.target.value
+      })
+    }
 
-//     handleChange = (e, { value }) => this.setState({ value })
+    onAddressChange = (event) => {
+      this.setState({
+        streetAddress: event.target.value
+      })
+    }
 
-//     render() {
-//       const { value } = this.state
-//       return (
+    onCityChange = (event) => {
+      this.setState({
+        city: event.target.value
+      })
+    }
 
+    onStateChange = (event) => {
+      this.setState({
+        state: event.target.value
+      })
+    }
 
-//         <Form >
-//           <Form.Group widths={6}>
-//             <Form.Field control={Input} label='Event Name' placeholder='Event Name' />
-//             <Form.Field control={Input} label='Event Creator' placeholder='Event Creator' />
-//             <Form.Field control={Input} label='Venue Name' placeholder='Venue Name' />
+    onCountryChange = (event) => {
+      this.setState({
+        country: event.target.value
+      })
+    }
 
-//             <Form.Field control={Input} label='Time' placeholder='Time' />
-//             <Form.Field control={Input} label='Street Address' placeholder='Street Address' />
-//             <Form.Field control={Input} label='City' placeholder='City' />
-//             <Form.Field control={Input} label='State' placeholder='State' />
-//             <Form.Field control={Input} label='Country' placeholder='Country' />
-//             <Form.Field control={Input} label='Venue Name' placeholder='Venue Name' />
-//           </Form.Group>
+    onVenueNameChange = (event) => {
+      this.setState({
+        venueName: event.target.value
+      })
+    }
 
+    onAboutChange = (event) => {
+      this.setState({
+        about: event.target.value
+      })
+    }
 
-//           <Form.Field control={TextArea} label='About' placeholder='Tell us more about you...' />
-//           <Form.Field control={Checkbox} label='I agree to the Terms and Conditions' />
-//           <Button color = 'grey' className = "logout-button"  animated onClick = {this.onLogout}>
-//             <Button.Content visible>Create Event Go!</Button.Content>
-//                 <Button.Content hidden>
-//                     <Icon name='right arrow'   />
-//                 </Button.Content>
-//             </Button>
-//         </Form>
+      render() {
+        return (
+          <Form>
+            <Form.Group>
+            <Form.Field control={Input} label='Event Name' placeholder='Event Name' onChange={this.onEventNameChange} />
+            <Form.Field control={Input} label='Event Creator' placeholder='Event Creator' onChange={this.onEventCreatorChange} />
+          </Form.Group>
+            <DateInput
+              name="date"
+              inline
+              placeholder="Date"
+              value={this.state.date}
+              iconPosition="left"
+              onChange={this.handleChange} />
+              <br />
+            <TimeInput
+              inline
+              name="time"
+              placeholder="Time"
+              value={this.state.time}
+              iconPosition="left"
+              onChange={this.handleChange} />
+               <br />
 
-//       )
-//     }
-// }
+            <DatesRangeInput
+              inline
+              name="datesRange"
+              placeholder="From - To"
+              value={this.state.datesRange}
+              iconPosition="left"
+              onChange={this.handleChange} />
+              <Form.Group>
+             <Form.Field control={Input} label='Street Address' placeholder='Street Address' onChange={this.onAddressChange} />
+               <Form.Field control={Input} label='City' placeholder='City' onChange={this.onCityChange}/>
+               <br />
+               <br />
+               <Form.Field control={Input} label='State' placeholder='State'  onChange={this.onStateChange}/>
+               <Form.Field control={Input} label='Country' placeholder='Country' onChange={this.onCountryChange}/>
+               <Form.Field control={Input} label='Venue Name' placeholder='Venue Name' onChange={this.onVenueNameChange}/>
+               <Form.Field control={TextArea} label='About' placeholder='Tell us more about you...' onChange={this.onAboutChange} />
+              </Form.Group>
+              <Button style={{display:'flex', alignItems:'center'}} color = 'pink' className = "logout-button"  animated onClick = {this.onCreate}>
+               <Button.Content visible>Create Event Go!</Button.Content>
+                   <Button.Content hidden>
+                       <Icon name='right arrow'   />
+                   </Button.Content>
+               </Button>
+          </Form>
+        );
+      }
+    }
