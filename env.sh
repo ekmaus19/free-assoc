a04dfa674d5782d2b936888992f411ab1309d4ab
richaparande <37963560+richaparande@users.noreply.github.com>
2018-08-06 17:29:37 -0700

    Delete env.sh

--- env.sh
+++
@@ -1,3 +0,0 @@
-export MAPBOX_TOKEN="pk.eyJ1IjoiZWttYXVzMTkiLCJhIjoiY2prYTAyc3JvMXppbjNrbWtmNTI5cmFheSJ9.SRlzG8UvBjRsNKoB1oY56Q"
-export MONGODB_URI="mongodb://user:user123@ds117495.mlab.com:17495/free-assoc"
-export SECRET="qwerty"
