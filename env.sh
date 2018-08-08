81f8b5af35d5e02b8ae3fc9f46ac60489791a622
Liza Kravchenko <ekravchenko2019@gmail.com>
2018-08-08 14:51:44 -0700

    switching to ekmaus19

--- env.sh
+++
@@ -1,4 +0,0 @@
-export MAPBOX_TOKEN="pk.eyJ1IjoiZWttYXVzMTkiLCJhIjoiY2prYTAyc3JvMXppbjNrbWtmNTI5cmFheSJ9.SRlzG8UvBjRsNKoB1oY56Q"
-export MONGODB_URI="mongodb://amp:horizons7@ds117495.mlab.com:17495/free-assoc"
-export SECRET="qwerty"
-export NGROK_URL="http://7237f501.ngrok.io"
