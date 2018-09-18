b983a562b184a49a087f4a0542b4adb80005a393
richaparande <richaparande@hotmail.com>
2018-09-17 22:32:42 -0400

    stuff

--- backend/server.js
+++ backend/server.js
@@ -227,4 +227,4 @@ module.exports = app;
 
 server.listen(1337, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:1337/')
+console.log('Server running at https://powerful-bastion-26209.herokuapp.com/')
