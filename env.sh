148034e8cdcdcd45de4d650a618dda19cfc0799f
richaparande <richaparande@hotmail.com>
2018-10-30 16:12:23 -0400

    deploying

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 export MONGODB_URI="mongodb://amp:horizons1@ds117495.mlab.com:17495/free-assoc"
 export SECRET="qwerty"
 export GOOGLE_API_KEY="AIzaSyAs7riE2xT80wzGfYJq8SpjisLjDvSNeZA"
-export AWS_ACCESS_KEY_ID="AKIAIE42NZBS2VOF7I3A"
-export AWS_SECRET_ACCESS_KEY="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"
+export AWS_ACCESS_KEY_ID="AKIAIEHNYE4AZEWOBQCQ"
+export AWS_SECRET_ACCESS_KEY="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"
