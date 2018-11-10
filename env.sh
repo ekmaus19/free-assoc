8e029aca616c201ee703af87dfc9fe1d55a6114a
richaparande <37963560+richaparande@users.noreply.github.com>
2018-11-10 18:23:31 -0500

    Delete env.sh

--- env.sh
+++
@@ -1,5 +0,0 @@
-export MONGODB_URI="mongodb://amp:horizons1@ds117495.mlab.com:17495/free-assoc"
-export SECRET="qwerty"
-export GOOGLE_API_KEY="AIzaSyAs7riE2xT80wzGfYJq8SpjisLjDvSNeZA"
-export AWS_ACCESS_KEY_ID="AKIAJHJ7ETAOZAPDB22Q"
-export AWS_SECRET_ACCESS_KEY="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"
