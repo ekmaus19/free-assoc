6b440e585777c29b78d7ca1eaefdfb1d00816f18
Tundun Oladipo <tundunoladipo@gmail.com>
2019-02-03 21:24:46 -0800

    Update cors origin

--- backend/server.js
+++ backend/server.js
@@ -120,7 +120,7 @@ app.use(validator());
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://powerful-bastion-26209.herokuapp.com"
+    "https://dl5kq0lhnuasf.cloudfront.net"
   );
   res.header(
     "Access-Control-Allow-Headers",
